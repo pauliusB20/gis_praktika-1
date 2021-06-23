ab04ecc91ca37f23ee6dca5b9a71b8ff90f83797
Paulius Balčiūnas <studvuit.pauliusbalciunas@gmail.com>
2021-06-23 19:47:43 +0300

    laravel template for the app

---
+++ src/arc_app/resources/views/apppage.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @section('headline')
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <h1>ARCGIS Test APP</h1>
+
+
+
+            </div>
+        </div>
+    </body>
+</html>
