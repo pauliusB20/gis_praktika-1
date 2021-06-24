af0979d0f8b61cb380361d4acc6b11216fefe504
Paulius Balčiūnas <studvuit.pauliusbalciunas@gmail.com>
2021-06-24 12:35:05 +0300

    Add some popups

--- src/arc_app/resources/views/apppage.blade.php
+++ src/arc_app/resources/views/apppage.blade.php
@@ -20,29 +20,129 @@
       
         <!-- Main map code -->
         <script>
-            require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+            require([
+                     "esri/config",
+                     "esri/Map", 
+                     "esri/views/MapView",
+                     "esri/layers/FeatureLayer"
+                     ], 
+                     function (esriConfig, Map, MapView, FeatureLayer) {
 
-                esriConfig.apiKey = "AAPK7c7895bf808a4f23b5d9f049944cebbdRBkAqsrkczQn96GKC9hWASWXII9K0ZIVOf8r2PUZgA8KfrpB4cYBBgUkZYwlWHo9";
+                        esriConfig.apiKey = "AAPK7c7895bf808a4f23b5d9f049944cebbdRBkAqsrkczQn96GKC9hWASWXII9K0ZIVOf8r2PUZgA8KfrpB4cYBBgUkZYwlWHo9";
 
-                const map = new Map({
-                            basemap: "arcgis-topographic" // Basemap layer service
-                            });
+                        const map = new Map({
+                                    basemap: "arcgis-topographic" // Basemap layer service
+                                    });
 
-                const view = new MapView({
-                                map: map,
-                                center: [-118.805, 34.027], // Longitude, latitude
-                                zoom: 13, // Zoom level
-                                container: "viewDiv" // Div element
-                            });
-            // console.log(view);
-                view.when(function(){
-                // All the resources in the MapView and the map have loaded. Now execute additional processes
-                    console.log("I have some data!");
-                }, function(error){
-                    // Use the errback function to handle when the view doesn't load properly
-                     console.log("The view's resources failed to load: ", error);
-                });
-            });
+                        const view = new MapView({
+                                        map: map,
+                                        center: [-118.80543,34.02700], // Longitude, latitude
+                                        zoom: 13, // Zoom level
+                                        container: "viewDiv" // Div element
+                                    });
+
+                        const popupTrailheads = {
+                            "title": "Information pop up",
+                            "content": "<b>Trail:</b> {TRL_NAME}<br><b>City:</b> {CITY_JUR}<br><b>Cross Street:</b> {X_STREET}<br><b>Parking:</b> {PARKING}<br><b>Elevation:</b> {ELEV_FT} ft"
+                        }
+                        const trailheads = new FeatureLayer({
+                            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0",
+                            outFields: ["TRL_NAME","CITY_JUR","X_STREET","PARKING","ELEV_FT"],
+                            popupTemplate: popupTrailheads
+                        });
+
+                        map.add(trailheads);
+
+                        const popupTrails = {
+                            title: "Trail Information",
+                            content: [{
+                            type: "media",
+                            mediaInfos: [{
+                                type: "pie-chart",
+                                caption: "",
+                                value: {
+                                fields: [ "ELEV_MIN","ELEV_MAX" ],
+                                normalizeField: null,
+                                tooltipField: "Min and max elevation values"
+                                }
+                                }]
+                            }]
+                        }
+                        const trails = new FeatureLayer({
+                            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails_Styled/FeatureServer/0",
+                            outFields: ["TRL_NAME","ELEV_GAIN"],
+                            popupTemplate: popupTrails
+                        });
+
+                        map.add(trails,0);
+                        
+                        // Define popup for Parks and Open Spaces
+                        const popupOpenspaces = {
+                            "title": "{PARK_NAME}",
+                            "content": [{
+                            "type": "fields",
+                            "fieldInfos": [
+                                {
+                                "fieldName": "AGNCY_NAME",
+                                "label": "Agency",
+                                "isEditable": true,
+                                "tooltip": "",
+                                "visible": true,
+                                "format": null,
+                                "stringFieldOption": "text-box"
+                                },
+                                {
+                                "fieldName": "TYPE",
+                                "label": "Type",
+                                "isEditable": true,
+                                "tooltip": "",
+                                "visible": true,
+                                "format": null,
+                                "stringFieldOption": "text-box"
+                                },
+                                {
+                                "fieldName": "ACCESS_TYP",
+                                "label": "Access",
+                                "isEditable": true,
+                                "tooltip": "",
+                                "visible": true,
+                                "format": null,
+                                "stringFieldOption": "text-box"
+                                },
+
+                                {
+                                "fieldName": "GIS_ACRES",
+                                "label": "Acres",
+                                "isEditable": true,
+                                "tooltip": "",
+                                "visible": true,
+                                "format": {
+                                    "places": 2,
+                                    "digitSeparator": true
+                                },
+
+                                "stringFieldOption": "text-box"
+                                }
+                            ]
+                            }]
+                        }
+
+                        const openspaces = new FeatureLayer({
+                            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space_Styled/FeatureServer/0",
+                            outFields: ["TYPE","PARK_NAME", "AGNCY_NAME","ACCESS_TYP","GIS_ACRES","TRLS_MI","TOTAL_GOOD","TOTAL_FAIR", "TOTAL_POOR"],
+                            popupTemplate: popupOpenspaces
+                        });
+
+                        map.add(openspaces,0);
+                    // console.log(view);
+                        view.when(function(){
+                        // All the resources in the MapView and the map have loaded. Now execute additional processes
+                            console.log("I have some data!");
+                        }, function(error){
+                            // Use the errback function to handle when the view doesn't load properly
+                            console.log("The view's resources failed to load: ", error);
+                        });
+                     });
          </script>
         <!-- Styles -->
         <style>
