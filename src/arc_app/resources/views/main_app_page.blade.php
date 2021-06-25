9fd2836aa7cdcd4ea7231c6fd975a17d273b9f5a
Paulius Balčiūnas <studvuit.pauliusbalciunas@gmail.com>
2021-06-25 16:20:08 +0300

    fixed title

--- src/arc_app/resources/views/main_app_page.blade.php
+++ src/arc_app/resources/views/main_app_page.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-        <title>ARCGIS APP  @yield('title')</title>
+        <title>MEGISA - main page</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
